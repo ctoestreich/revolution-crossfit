3a038212a0a59a8bbd114e97c142dda7e2ccebba
Christian <acetrike@yahoo.com>
2013-08-11 22:35:22 -0500

    Initial Commit

---
+++ grails-app/conf/ApplicationResources.groovy
@@ -0,0 +1,15 @@
+modules = {
+  application {
+    dependsOn 'jquery', 'bootstrap', 'bootstrap-responsive-css', 'font-awesome'
+
+    resource url: 'css/custom.css'
+    resource url: 'css/flexslider.css'
+    resource url: 'css/style.css'
+    resource url: 'css/color-blue.css'
+
+    resource url: 'js/jquery.quicksand.js'
+    resource url: 'js/jquery.flexslider-min.js'
+    resource url: 'js/jquery.stellar.min.js'
+    resource url: 'js/script.js'
+  }
+}
